d71af18d7fd589c685aa3d26c0d5fc8e707098ec
Johnny Nez <nezennin@passworMacBook3.attlocal.net>
2015-07-29 11:51:47 -0400

    Initial Commit

---
+++ fiuparkingfinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fiuparkingfinder
+//
+//  Created by Johnny Nez on 7/29/15.
+//  Copyright (c) 2015 Johnny Nez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ fiuparkingfinder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  fiuparkingfinder
+//
+//  Created by Johnny Nez on 7/29/15.
+//  Copyright (c) 2015 Johnny Nez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
